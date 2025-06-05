828290874f7b8843acf76f59098b743b35568b64
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-06-05 15:42:43 +0200

    feat : add and import the html in the app files

--- brief-contact-a/src/app/app.ts
+++ brief-contact-a/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchBar } from './search-bar/search-bar';
 import { Header } from './header/header';
+import { Card } from './card/card';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, SearchBar],
+  imports: [RouterOutlet, Header, SearchBar, Card],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
