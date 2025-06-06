4b6db5f0a660ebd6d8c845c92b960af908b60ac2
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-06-06 17:00:48 +0200

    feat : adding the folder data and the data file

--- brief-contact-a/src/app/app.ts
+++ brief-contact-a/src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { OriginLayout } from './origin-layout/origin-layout';
-import { Footer } from './footer/footer';
+import { Card } from './card/card';
+import { TEAM_MEMBERS } from './data/personne.data';
 
 @Component({
   selector: 'app-root',
-  imports: [OriginLayout, Footer],
+  imports: [OriginLayout, Card],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'brief-contact-a';
+  personne = TEAM_MEMBERS;
 }
--- brief-contact-a/src/app/card/card.ts
+++ brief-contact-a/src/app/card/card.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { TEAM_MEMBERS } from '../data/personne.data';
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.html',
   styleUrls: ['./card.css']
 })
 export class Card {
-
+ team = TEAM_MEMBERS;
 }
