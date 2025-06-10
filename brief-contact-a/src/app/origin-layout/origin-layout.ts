13257ddc8243d186b524caf7fa944813043e01c1
symbios <julien.sarkarin@gmail.com>
2025-06-10 10:00:34 +0200

    feat: add call to card in origin layout

--- brief-contact-a/src/app/origin-layout/origin-layout.ts
+++ brief-contact-a/src/app/origin-layout/origin-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Router, RouterOutlet } from '@angular/router';
+import { Card } from '../card/card';
 
 @Component({
   selector: 'app-origin-layout',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, Card],
   templateUrl: './origin-layout.html',
   styleUrl: './origin-layout.css',
 })
