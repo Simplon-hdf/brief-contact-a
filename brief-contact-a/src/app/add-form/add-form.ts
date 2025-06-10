aaee8659192879ebffc657f9aa7c0a4b0cfb2ce4
symbios <julien.sarkarin@gmail.com>
2025-06-10 13:49:08 +0200

    feat: form design and translation, add a selector for pictures

--- brief-contact-a/src/app/add-form/add-form.ts
+++ brief-contact-a/src/app/add-form/add-form.ts
@@ -4,8 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-add-form',
   imports: [],
   templateUrl: './add-form.html',
-  styleUrl: './add-form.css'
+  styleUrl: './add-form.css',
 })
 export class AddForm {
+  constructor() {}
 
+  imagePreview: string =
+    'https://lh3.googleusercontent.com/a-/AFdZucpC_6WFBIfaAbPHBwGM9z8SxyM1oV4wB4Ngwp_UyQ=s96-c';
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+
+    if (file) {
+      this.imagePreview = URL.createObjectURL(file);
+    }
+  }
 }
