70c17ad4d86b2b7b0adc59ea62ea8ccbb67489e1
SymBiios <julien.sarkarin@gmail.com>
2025-06-11 12:07:15 +0200

    feat: add card of search bar now redirect on profile

--- brief-contact-a/src/app/search-bar/search-bar.ts
+++ brief-contact-a/src/app/search-bar/search-bar.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Member, TeamMember } from '../member';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -13,11 +14,15 @@ export class SearchBar implements OnInit {
   searchResults: TeamMember[] = [];
   searchTerm: string = '';
 
-  constructor(private member: Member) {}
+  constructor(private member: Member, private router: Router) {}
 
   ngOnInit(): void {
     this.allMembers = this.member.getAllMembers();
     this.searchResults = [];
+  }
+
+  goToProfile(memberName: string) {
+    this.router.navigate(['/profile', memberName]);
   }
 
   onSearch(event: any) {
